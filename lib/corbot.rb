c81b160bf97c9f9e74a9bdc4493c31ec5bdc6cc1
Christian Blavier <cblavier@gmail.com>
2019-11-22 12:18:33 +0100

    .gitignore

--- lib/corbot.rb
+++ lib/corbot.rb
@@ -1,8 +1,17 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 
-
-
 get '/ping' do
   'pong'
+end
+
+post '/' do
+  case params[:text].trim
+  when "help"
+    "coucou"
+  when "ping"
+    "pong"
+  else
+    params.inspect
+  end
 end
